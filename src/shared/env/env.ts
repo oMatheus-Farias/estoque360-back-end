15eba2775424108aad717e25938d363954f973c3
Matheus <matheusfariasdasneves@gmail.com>
2025-06-28 18:45:48 -0300

    chore: add prisma config and first version of the prisma schema

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -5,6 +5,7 @@ import { z } from 'zod';
 const schema = z.object({
   NODE_ENV: z.enum(['development', 'homolog', 'production', 'test']).default('development'),
   PORT: z.coerce.number().default(3001),
+  DATABASE_URL: z.string().min(1),
 });
 
 export const env = schema.parse(process.env);
