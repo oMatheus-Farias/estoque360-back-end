d62151de21ab1248aa55841761cd93c3599d076d
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-07-10 18:18:20 -0300

    feat: add initial config of google authenticated

--- src/main/app.ts
+++ src/main/app.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata';
 
 import { ErrorCode } from '@application/errors/ErrorCode';
 import { fastifyCors } from '@fastify/cors';
+// import fastifyPassport from '@fastify/passport';
+// import { prisma } from '@infra/clients/prismaClient';
 import { env } from '@shared/env/env';
 import { accountRoutes } from '@web/routes/accountRoutes';
 import { fastify } from 'fastify';
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { ZodError } from 'zod';
 
 export const app = fastify();
@@ -13,6 +16,39 @@ app.register(fastifyCors, {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 });
+
+// fastifyPassport.use(
+//   'google',
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//       callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         let user = await prisma.user.findUnique({
+//           where: { googleId: profile.id },
+//         });
+
+//         if (!user) {
+//           user = await prisma.user.create({
+//             data: {
+//               email: profile.emails?.[0].value || '',
+//               name: profile.displayName,
+//               googleId: profile.id,
+//               picture: profile.photos?.[0].value,
+//             },
+//           });
+//         }
+
+//         done(null, user);
+//       } catch (err) {
+//         done(err as any, undefined);
+//       }
+//     },
+//   ),
+// );
 
 // heath check route
 app.get('/health', () => {
