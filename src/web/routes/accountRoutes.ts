c30f12ccdae97549a2d20c0a901a3be95683baf0
Matheus <matheusfariasdasneves@gmail.com>
2025-07-17 16:00:39 -0300

    feat: add get account details endpoint

--- src/web/routes/accountRoutes.ts
+++ src/web/routes/accountRoutes.ts
@@ -1,9 +1,22 @@
 import { fastifyAdapter } from '@application/adapters/fastifyAdapters';
 import { Registry } from '@kermel/di/Registry';
 import { CreateAccountController } from '@web/http/controllers/accounts/AccountController';
+import { GetAccountDetailsController } from '@web/http/controllers/accounts/GetAccountDetailsController';
+import { verifyJwt } from '@web/middlewares/verifyJwt';
 import type { FastifyInstance } from 'fastify';
 
 export async function accountRoutes(app: FastifyInstance) {
-  const createAccountController = Registry.getInstance().resolve(CreateAccountController);
+  const registry = Registry.getInstance();
+  const createAccountController = registry.resolve(CreateAccountController);
+  const getAccountDetailsController = registry.resolve(GetAccountDetailsController);
+
   app.post('/', fastifyAdapter(createAccountController));
+
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJwt],
+    },
+    fastifyAdapter(getAccountDetailsController),
+  );
 }
