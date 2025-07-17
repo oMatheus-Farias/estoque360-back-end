0accd88a143d45531d7bdb5dabd3c4a469b67936
Matheus <matheusfariasdasneves@gmail.com>
2025-07-17 15:26:12 -0300

    feat: add sign out endpoint

--- src/web/routes/sessionRoutes.ts
+++ src/web/routes/sessionRoutes.ts
@@ -1,9 +1,13 @@
 import { fastifyAdapter } from '@application/adapters/fastifyAdapters';
 import { Registry } from '@kermel/di/Registry';
 import { CredentialsAuthController } from '@web/http/controllers/sessions/CredentialsAuthController';
+import { SignOutController } from '@web/http/controllers/sessions/SignOutController';
+import { verifyJwt } from '@web/middlewares/verifyJwt';
 import type { FastifyInstance } from 'fastify';
 
 export async function sessionRoutes(app: FastifyInstance) {
   const credentialsAuthController = Registry.getInstance().resolve(CredentialsAuthController);
+  const signOutController = Registry.getInstance().resolve(SignOutController);
   app.post('/credentials', fastifyAdapter(credentialsAuthController));
+  app.post('/sign-out', { onRequest: [verifyJwt] }, fastifyAdapter(signOutController));
 }
