3999a9744ac2f4c66bb24cda1d70372fc79ad610
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-07-18 13:12:11 -0300

    feat: add update company endpoint

--- src/web/routes/companyRoutes.ts
+++ src/web/routes/companyRoutes.ts
@@ -1,10 +1,15 @@
 import { fastifyAdapter } from '@application/adapters/fastifyAdapters';
 import { Registry } from '@kermel/di/Registry';
 import { CreateCompanyController } from '@web/http/controllers/companies/CreateCompanyController';
+import { UpdateCompanyController } from '@web/http/controllers/companies/UpdateCompanyController';
 import { verifyJwt } from '@web/middlewares/verifyJwt';
 import type { FastifyInstance } from 'fastify';
 
 export async function companyRoutes(app: FastifyInstance) {
   const createCompanyController = Registry.getInstance().resolve(CreateCompanyController);
+  const updateCompanyController = Registry.getInstance().resolve(UpdateCompanyController);
+
   app.post('/', { onRequest: [verifyJwt] }, fastifyAdapter(createCompanyController));
+
+  app.patch('/:companyId', { onRequest: [verifyJwt] }, fastifyAdapter(updateCompanyController));
 }
