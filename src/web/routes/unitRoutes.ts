80278227a83d58c7338edc3de25bfb5277e00ca2
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-07-21 15:14:53 -0300

    feat: add update unit endpoint

--- src/web/routes/unitRoutes.ts
+++ src/web/routes/unitRoutes.ts
@@ -1,11 +1,15 @@
 import { fastifyAdapter } from '@application/adapters/fastifyAdapters';
 import { Registry } from '@kermel/di/Registry';
 import { CreateUnitController } from '@web/http/controllers/units/CreateUnitController';
+import { UpdateUnitController } from '@web/http/controllers/units/UpdateUnitController';
 import { verifyJwt } from '@web/middlewares/verifyJwt';
 import type { FastifyInstance } from 'fastify';
 
 export async function unitRoutes(app: FastifyInstance) {
   const createUnitController = Registry.getInstance().resolve(CreateUnitController);
+  const updateUnitController = Registry.getInstance().resolve(UpdateUnitController);
 
   app.post('/', { onRequest: [verifyJwt] }, fastifyAdapter(createUnitController));
+
+  app.patch('/:unitId', { onRequest: [verifyJwt] }, fastifyAdapter(updateUnitController));
 }
